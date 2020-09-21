2a64a6d76abe0edb1e9c63e217c54372d9c0f420
moqi <moqimoqidea@gmail.com>
2020-09-21 16:18:07 +0800

    fireAndForget

--- src/main/java/com/moqi/Producer01.java
+++ src/main/java/com/moqi/Producer01.java
@@ -1,19 +1,22 @@
 package com.moqi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 /**
  * @author moqi
  * On 9/21/20 15:50
  */
-
+@Slf4j
 public class Producer01 {
 
-    public static void main(String[] args) {
-        fireAndForget();
+    public static void main(String[] args) throws Exception {
+        syncSend();
     }
 
     private static KafkaProducer<String, String> getProducer() {
@@ -33,6 +36,19 @@ public class Producer01 {
         ProducerRecord<String, String> record = new ProducerRecord<>("test", "Precision Products", "France");
 
         producer.send(record);
+    }
+
+    /**
+     * 同步发送
+     */
+    private static void syncSend() throws ExecutionException, InterruptedException {
+        KafkaProducer<String, String> producer = getProducer();
+        ProducerRecord<String, String> record = new ProducerRecord<>("test", "Precision Products", "France");
+
+        RecordMetadata recordMetadata = producer.send(record).get();
+        long offset = recordMetadata.offset();
+        int partition = recordMetadata.partition();
+        log.info("offset:{}, partition:{}", offset, partition);
     }
 
 }
